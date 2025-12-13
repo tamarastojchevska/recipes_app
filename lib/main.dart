91eeadeb0850b3eeb45c41d5b84fe43f5de6571c
stojchevska <138624193+tamarastojchevska@users.noreply.github.com>
2025-12-13 11:35:48 +0100

    Initialize Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:recipes_app/screens/home.dart';
-import 'package:recipes_app/screens/recipe.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
